f1480da8de040c462dbce0b441290be2670d719a
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-10-30 14:10:53 +0530

    6s

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import HeadingContent from "./Components/HeadingContent";
 import MainCard from "./Components/MainCard";
-import FirstPage from "./Components/FirstPage";
-import Brands from "./Components/Brands";
-import Profile from "./Components/Profile";
-import SliderGallery from "./Components/SliderGallery";
-import SliderGalleryReverse from "./Components/SliderGalleryReverse";
-import Testimonial from "./Components/Testimonial";
-import NewDash from "./Components/NewDash";
-import Quote from "./Components/Quote";
-import Footer from "./Components/Footer";
-import Contact from "./Components/Contact";
 import "animate.css";
 
 const App = () => {
