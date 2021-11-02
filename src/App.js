e1335d2fddf527c843f95599539c9f02ccdeed64
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-02 15:27:34 +0530

    gallary

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
 import MainCard from "./Components/MainCard";
+import SliderGallery from "./Components/SliderGallery";
+import SliderGalleryReverse from "./Components/SliderGalleryReverse";
 import "animate.css";
 
 const App = () => {
   return (
     <>      
       <MainCard />
+      <SliderGallery />
+      <SliderGalleryReverse />
     </>
   );
 };
---
+++ src/Components/SliderGalleryReverse.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const SliderGalleryReverse = () => {
+  return (
+    <>
+      <div className="" style={{ position: "relative", top: "-60px" }}>
+        <div class="slider">
+          <div class="slide-track1">
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+            <div class="slide2"></div>
+          </div>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .slider {
+            background-color: #2b0505;
+           
+          }
+          .slide2 {
+            margin-left: 10px;
+            margin-right: 10px;
+            border-radius: 20px;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default SliderGalleryReverse;
